b60aca2f96dbccf07fd7b4adcced48aca9e50cea
Lucas <lucas@ecoding.com.br>
2021-04-04 00:47:51 -0300

    add rating model and factories

--- app/models/frequent_question.rb
+++ app/models/frequent_question.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class FrequentQuestion < ApplicationRecord
+  has_many :ratings, as: :rated
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.unique.email }
+    document_id { Faker::CPF.numeric }
+    password { Faker::Alphanumeric.alphanumeric(number: 10) }
+  end
+end
