6d0c94fc6ba93377ef75f54d601c5fc6cba7d95a
Lucas <lucas@ecoding.com.br>
2021-05-15 07:54:27 -0300

    update

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -4,7 +4,8 @@ require 'devise/jwt/test_helpers'
 
 module ApiHelper
   def api_sign_in(user)
-    auth_headers = Devise::JWT::TestHelpers.auth_headers({}, user)
+    headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+    auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
     auth_headers.each { |key, value| request.headers[key] = value }
   end
 end
